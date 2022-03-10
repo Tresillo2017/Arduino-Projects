a4e44329ef30b985e5f663a19fb0beb75d228f8b
Tresillocrack <tresillo2017@gmail.com>
2022-03-10 12:11:23 +0100

    Circuito 1

---
+++ circuito 1.ino
@@ -0,0 +1,25 @@
+/*
+Enlace Tinkercad
+https://www.tinkercad.com/things/9sMqw70Y9Hk-copy-of-semaforo-1/editel?sharecode=37aNWS8SyPC3EKnuJE9e59TauVp-96WMBPo85Le2j7k
+*/
+
+
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  delay(4000); // Espera por 4 segundo
+  digitalWrite(8, LOW); 
+  digitalWrite(6, HIGH);
+  delay (3000); //Espera por 3 segundos 
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW); 
+  digitalWrite(7, HIGH);
+  delay(2000); // Espera por 2 segundos
+}
