b746a20565790ad865ec41ab93b0b774cefb957e
Tresillocrack <tresillo2017@gmail.com>
2022-03-21 13:59:48 +0100

    Create circuito 5.ino

---
+++ circuito 5.ino
@@ -0,0 +1,45 @@
+#define led1 7
+#define led2 2
+#define led3 8
+#define led4 4
+
+/* 
+Enlace Tinkercad
+https://www.tinkercad.com/things/bZdwqdBzeEj-brave-curcan-kasi/editel?sharecode=2fTFN41US1NrThV--KHrreWv1gnOGeK6Sn2pWLd2dWQ
+*/
+
+#define tiempo 500
+
+#define boton 1
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(boton, INPUT);
+}
+
+void loop()
+{
+	while(digitalRead(boton) == HIGH)
+    {
+      digitalWrite(led1, HIGH);
+      delay(tiempo);
+      digitalWrite(led1, LOW);
+      delay(tiempo);
+      digitalWrite(led2, HIGH);
+      delay(tiempo);
+      digitalWrite(led2, LOW);
+    }
+  	while(digitalRead(boton) == LOW)
+    {
+      digitalWrite(led3, HIGH);
+      delay(tiempo);
+      digitalWrite(led3, LOW);
+      delay(tiempo);
+      digitalWrite(led4, HIGH);
+      delay(tiempo);
+      digitalWrite(led4, LOW);
+    }
+}
